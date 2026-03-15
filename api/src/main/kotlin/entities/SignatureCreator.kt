bc2286fd2cd631ae5bcad8a49b99de964a79aac4
Tomas093 <tjmonteiro@mail.austral.edu.ar>
2026-03-14 21:07:03 -0300

    feat: update signature creation to use ECDSA and add public key conversion utility

--- api/src/main/kotlin/entities/SignatureCreator.kt
+++ api/src/main/kotlin/entities/SignatureCreator.kt
@@ -1,15 +1,17 @@
 package api.entities
 
+import java.security.KeyFactory
 import java.security.PrivateKey
 import java.security.PublicKey
 import java.security.Signature
+import java.security.spec.X509EncodedKeySpec
 import java.util.Base64
 
 fun createSignature(
     privateKey: PrivateKey,
     message: String,
 ): String {
-    val s = Signature.getInstance("SHA256withRSA")
+    val s = Signature.getInstance("SHA256withECDSA")
     s.initSign(privateKey)
     s.update(message.toByteArray())
     val signatureBytes = s.sign()
@@ -21,9 +23,16 @@ fun verifySignature(
     message: String,
     signature: String,
 ): Boolean {
-    val s = Signature.getInstance("SHA256withRSA")
+    val s = Signature.getInstance("SHA256withECDSA")
     s.initVerify(publicKey)
     s.update(message.toByteArray())
     val signatureBytes = Base64.getDecoder().decode(signature)
     return s.verify(signatureBytes)
+}
+
+fun getPublicKeyFromString(keyString: String): PublicKey {
+    val publicBytes = Base64.getDecoder().decode(keyString)
+    val keySpec = X509EncodedKeySpec(publicBytes)
+    val keyFactory = KeyFactory.getInstance("EC")
+    return keyFactory.generatePublic(keySpec)
 }
--- api/src/main/kotlin/entities/transaction/rules/ValidCryptographicSignatureRule.kt
+++ api/src/main/kotlin/entities/transaction/rules/ValidCryptographicSignatureRule.kt
@@ -1,36 +1,17 @@
 package api.entities.transaction.rules
 
 import api.dtos.Transaction
-import java.security.PublicKey
-import java.security.Signature
-import java.util.Base64
+import api.entities.getPublicKeyFromString
+import api.entities.verifySignature
 
 class ValidCryptographicSignatureRule : TransactionRule {
 
     override fun isValid(transaction: Transaction): Boolean =
         verifySignature(
-            publicKey = getPublicKeyFromString(transaction.to),
+            publicKey = getPublicKeyFromString(transaction.from),
             message = transaction.from + transaction.to + transaction.amount,
             signature = transaction.signature,
         )
 
-    override fun getErrorMessage(): String {
-        TODO("Not yet implemented")
-    }
-
-    fun verifySignature(
-        publicKey: PublicKey,
-        message: String,
-        signature: String,
-    ): Boolean {
-        val s = Signature.getInstance("SHA256withRSA")
-        s.initVerify(publicKey)
-        s.update(message.toByteArray())
-        val signatureBytes = Base64.getDecoder().decode(signature)
-        return s.verify(signatureBytes)
-    }
-
-    private fun getPublicKeyFromString(keyString: String): PublicKey {
-        TODO("Implementar conversión de String Base64/Hex a objeto PublicKey de Java")
-    }
+    override fun getErrorMessage(): String = "The signature of the transaction is not valid"
 }
